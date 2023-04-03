206e03f33a5fff7217f1c06180763e1756968493
Simon ågren <seoagren@gmail.com>
2023-04-03 12:38:57 +0200

    feat: add basic leaflet map

---
+++ pages/Map.tsx
@@ -0,0 +1,29 @@
+import dynamic from "next/dynamic";
+import { useState } from "react";
+import { MapContainer, TileLayer, Marker, useMap, Popup } from "react-leaflet";
+
+const Map = () => {
+  const [geoData, setGeoData] = useState({ lat: 64.536634, lng: 16.779852 });
+
+  const center = [geoData.lat, geoData.lng];
+
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{ height: "100vh" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+export default Map;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,11 @@ import { GetStaticProps } from "next";
 import Layout from "../components/Layout";
 import Post, { PostProps } from "../components/Post";
 import prisma from "../lib/prisma";
+import "leaflet/dist/leaflet.css";
 
+import dynamic from "next/dynamic";
+
+/**
 export const getStaticProps: GetStaticProps = async () => {
   const feed = await prisma.post.findMany({
     where: { published: true },
@@ -18,40 +22,17 @@ export const getStaticProps: GetStaticProps = async () => {
     revalidate: 10,
   };
 };
-
+*/
 type Props = {
   feed: PostProps[];
 };
 
-const Blog: React.FC<Props> = (props) => {
-  return (
-    <Layout>
-      <div className="page">
-        <h1>Public Feed</h1>
-        <main>
-          {props.feed.map((post) => (
-            <div key={post.id} className="post">
-              <Post post={post} />
-            </div>
-          ))}
-        </main>
-      </div>
-      <style jsx>{`
-        .post {
-          background: white;
-          transition: box-shadow 0.1s ease-in;
-        }
+const Blog = () => {
+  const MapWithNoSSR = dynamic(() => import("./Map"), {
+    ssr: false,
+  });
 
-        .post:hover {
-          box-shadow: 1px 1px 3px #aaa;
-        }
-
-        .post + .post {
-          margin-top: 2rem;
-        }
-      `}</style>
-    </Layout>
-  );
+  return <MapWithNoSSR></MapWithNoSSR>;
 };
 
 export default Blog;
