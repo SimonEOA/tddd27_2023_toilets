72948a38fb8a0ada9efa2fcf2171773228a49660
Simon ågren <seoagren@gmail.com>
2023-04-14 10:17:03 +0200

    feat: change location of zoomcontrols

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, ZoomControl } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 import { Markers } from "./Markers/Markes";
@@ -19,12 +19,20 @@ const Map = ({ width, height }: { width: string; height: string }) => {
         center={[geoData.lat, geoData.lng]}
         zoom={13}
         style={{ width: width, height: height }}
+        placeholder={
+          <p>
+            Map.{" "}
+            <noscript>You need to enable JavaScript to see this map.</noscript>
+          </p>
+        }
+        zoomControl={false}
       >
         <TileLayer
           attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
           url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
         />
         <Markers add={addMarker} />
+        <ZoomControl position="bottomright" />
       </MapContainer>
       <Button w={"100%"} mt="5px" onClick={() => setAddMarker((cur) => !cur)}>
         {!addMarker ? "Add markers" : "Stop adding markers"}
